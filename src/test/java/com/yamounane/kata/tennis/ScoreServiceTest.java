01889706a2903eb96175c5f354e29d3f6728fc27
Yassine Amounane <yassine.am@gmail.com>
2018-01-26 16:13:32 +0100

    ADD test cases for ScoreService.score() : red

--- src/test/java/com/yamounane/kata/tennis/ScoreServiceTest.java
+++ src/test/java/com/yamounane/kata/tennis/ScoreServiceTest.java
@@ -1,10 +1,14 @@
 package com.yamounane.kata.tennis;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.BlockJUnit4ClassRunner;
 
+import com.yamounane.kata.tennis.exception.ScoreException;
 import com.yamounane.kata.tennis.model.Player;
 import com.yamounane.kata.tennis.model.TennisParty;
 import com.yamounane.kata.tennis.service.ScoreService;
@@ -32,8 +36,25 @@ public class ScoreServiceTest {
 	}
 	
 	@Test
-	public void should_score_increase_when_player_score_for_party() {
+	public void should_score_increase_when_player_score_for_party() throws ScoreException {
+		scoreService.score(rollandGarrosFinal, nadal);
 		
+		assertThat(scoreService.getScoreFor(rollandGarrosFinal)).isEqualTo("Rafael Nadal | 15 | 0\n" + 
+																			"Roger Federer | 0 | 0");
+	}
+	
+	@Test
+	public void should_raise_exception_when_scoring_for_null_player() throws ScoreException {
+		assertThatThrownBy(() -> scoreService.score(rollandGarrosFinal, null)).isInstanceOf(ScoreException.class);
+	}
+	
+	@Test
+	public void should_raise_exception_when_scoring_for_null_party() throws ScoreException {
+		assertThatThrownBy(() -> scoreService.score(null, federer)).isInstanceOf(ScoreException.class);
 	}
 
+	@Test
+	public void should_raise_exception_when_scoring_for_null_party_and_null_player() throws ScoreException {
+		assertThatThrownBy(() -> scoreService.score(null, null)).isInstanceOf(ScoreException.class);
+	}
 }
