605ad83dd93f7a4e39ace3dd0b29d498e6290a32
Yassine Amounane <yassine.am@gmail.com>
2018-01-28 05:31:36 +0100

    ADD test case for get score : test green

--- src/main/java/com/yamounane/kata/tennis/service/ScoreServiceImpl.java
+++ src/main/java/com/yamounane/kata/tennis/service/ScoreServiceImpl.java
@@ -64,7 +64,40 @@ public class ScoreServiceImpl implements ScoreService {
 
 	@Override
 	public String getScoreFor(TennisParty party) throws ScoreException {
-		return null;
+		if (party == null) {
+			throw new ScoreException("Unable to get Score for party : given party is null!");
+		}
+
+		String score = party.getPlayerOne().getName() + "/" + party.getPlayerTwo().getName() + " : "
+				+ formatGameScore(party.getPlayerOne().getGame().getScore()) + "-"
+				+ formatGameScore(party.getPlayerTwo().getGame().getScore());
+
+		for (int i = 0; i < party.getPlayerOne().getSets().size() && i < party.getPlayerTwo().getSets().size(); i++) {
+			score += " | " + party.getPlayerOne().getSets().get(i).getScore() + "/"
+					+ party.getPlayerTwo().getSets().get(i).getScore();
+		}
+
+		if (party.isFinished()) {
+			Player winner = party.getPlayerOne().isWinner() ? party.getPlayerOne() : party.getPlayerTwo();
+			score += " - Winner is : " + winner.getName();
+		}
+
+		return score;
+	}
+
+	private String formatGameScore(int score) {
+		switch (score) {
+		case 1:
+			return "15";
+		case 2:
+			return "30";
+		case 3:
+			return "40";
+		case 4:
+			return "40(A)";
+		default:
+			return "0";
+		}
 	}
 
 }
--- src/test/java/com/yamounane/kata/tennis/ScoreServiceTest.java
+++ src/test/java/com/yamounane/kata/tennis/ScoreServiceTest.java
@@ -318,6 +318,189 @@ public class ScoreServiceTest {
 				.isInstanceOf(ScoreException.class);
 	}
 
+	@Test
+	public void should_get_score_for_party_game_score_get_15_0_when_printing_score_for_party() throws ScoreException {
+		scoreService.score(rollandGarrosFinal, nadal);
+
+		assertThat(nadal.getGame().getScore()).isEqualTo(1);
+		assertThat(nadal.getSets().get(0).getScore()).isEqualTo(0);
+		assertThat(nadal.getSets().get(0).isCurrent()).isEqualTo(true);
+		assertThat(federer.getGame().getScore()).isEqualTo(0);
+		assertThat(federer.getSets().get(0).getScore()).isEqualTo(0);
+		assertThat(federer.getSets().get(0).isCurrent()).isEqualTo(true);
+
+		assertThat(scoreService.getScoreFor(rollandGarrosFinal)).isEqualTo("Rafael Nadal/Roger Federer : 15-0 | 0/0");
+	}
+
+	@Test
+	public void should_get_score_for_party_game_score_get_40_30_when_printing_score_for_party() throws ScoreException {
+		scoreService.score(rollandGarrosFinal, nadal);
+		scoreService.score(rollandGarrosFinal, nadal);
+		scoreService.score(rollandGarrosFinal, nadal);
+		scoreService.score(rollandGarrosFinal, federer);
+		scoreService.score(rollandGarrosFinal, federer);
+
+		assertThat(nadal.getGame().getScore()).isEqualTo(3);
+		assertThat(nadal.getSets().get(0).getScore()).isEqualTo(0);
+		assertThat(nadal.getSets().get(0).isCurrent()).isEqualTo(true);
+		assertThat(federer.getGame().getScore()).isEqualTo(2);
+		assertThat(federer.getSets().get(0).getScore()).isEqualTo(0);
+		assertThat(federer.getSets().get(0).isCurrent()).isEqualTo(true);
+		assertThat(scoreService.getScoreFor(rollandGarrosFinal)).isEqualTo("Rafael Nadal/Roger Federer : 40-30 | 0/0");
+	}
+
+	@Test
+	public void should_get_score_for_party_game_score_get_40A_40_when_printing_score_for_party() throws ScoreException {
+		scoreService.score(rollandGarrosFinal, nadal);
+		scoreService.score(rollandGarrosFinal, nadal);
+		scoreService.score(rollandGarrosFinal, nadal);
+		scoreService.score(rollandGarrosFinal, federer);
+		scoreService.score(rollandGarrosFinal, federer);
+		scoreService.score(rollandGarrosFinal, federer);
+		scoreService.score(rollandGarrosFinal, nadal);
+
+		assertThat(nadal.getGame().getScore()).isEqualTo(4);
+		assertThat(nadal.getSets().get(0).getScore()).isEqualTo(0);
+		assertThat(nadal.getSets().get(0).isCurrent()).isEqualTo(true);
+		assertThat(federer.getGame().getScore()).isEqualTo(3);
+		assertThat(federer.getSets().get(0).getScore()).isEqualTo(0);
+		assertThat(federer.getSets().get(0).isCurrent()).isEqualTo(true);
+		assertThat(scoreService.getScoreFor(rollandGarrosFinal))
+				.isEqualTo("Rafael Nadal/Roger Federer : 40(A)-40 | 0/0");
+	}
+
+	@Test
+	public void should_get_score_for_party_game_score_get_6_0_when_printing_score_for_party() throws ScoreException {
+		repeat(24, () -> {
+			try {
+				scoreService.score(rollandGarrosFinal, federer);
+			} catch (ScoreException e) {
+				fail();
+			}
+		});
+
+		assertThat(nadal.getGame().getScore()).isEqualTo(0);
+		assertThat(nadal.getSets().get(0).getScore()).isEqualTo(0);
+		assertThat(nadal.getSets().get(1).getScore()).isEqualTo(0);
+		assertThat(nadal.getSets().get(0).isCurrent()).isEqualTo(false);
+		assertThat(nadal.getSets().get(1).isCurrent()).isEqualTo(true);
+		assertThat(federer.getGame().getScore()).isEqualTo(0);
+		assertThat(federer.getSets().get(0).getScore()).isEqualTo(6);
+		assertThat(federer.getSets().get(1).getScore()).isEqualTo(0);
+		assertThat(federer.getSets().get(0).isCurrent()).isEqualTo(false);
+		assertThat(federer.getSets().get(1).isCurrent()).isEqualTo(true);
+
+		assertThat(scoreService.getScoreFor(rollandGarrosFinal))
+				.isEqualTo("Rafael Nadal/Roger Federer : 0-0 | 0/6 | 0/0");
+	}
+
+	@Test
+	public void should_get_score_for_finished__party_when_player_win_6_0_and_0_6_and_6_0_games() throws ScoreException {
+		repeat(24, () -> {
+			try {
+				scoreService.score(rollandGarrosFinal, nadal);
+			} catch (ScoreException e) {
+				fail();
+			}
+		});
+		repeat(24, () -> {
+			try {
+				scoreService.score(rollandGarrosFinal, federer);
+			} catch (ScoreException e) {
+				fail();
+			}
+		});
+		repeat(24, () -> {
+			try {
+				scoreService.score(rollandGarrosFinal, nadal);
+			} catch (ScoreException e) {
+				fail();
+			}
+		});
+
+		assertThat(nadal.getGame().getScore()).isEqualTo(0);
+		assertThat(nadal.getSets().get(0).getScore()).isEqualTo(6);
+		assertThat(nadal.getSets().get(1).getScore()).isEqualTo(0);
+		assertThat(nadal.getSets().get(2).getScore()).isEqualTo(6);
+		assertThat(nadal.getSets().get(0).isCurrent()).isEqualTo(false);
+		assertThat(nadal.getSets().get(1).isCurrent()).isEqualTo(false);
+		assertThat(nadal.getSets().get(2).isCurrent()).isEqualTo(false);
+		assertThat(nadal.getSets().size()).isEqualTo(3);
+		assertThat(nadal.isWinner()).isEqualTo(true);
+		assertThat(federer.getGame().getScore()).isEqualTo(0);
+		assertThat(federer.getSets().get(0).getScore()).isEqualTo(0);
+		assertThat(federer.getSets().get(1).getScore()).isEqualTo(6);
+		assertThat(federer.getSets().get(2).getScore()).isEqualTo(0);
+		assertThat(federer.getSets().get(0).isCurrent()).isEqualTo(false);
+		assertThat(federer.getSets().get(1).isCurrent()).isEqualTo(false);
+		assertThat(federer.getSets().get(2).isCurrent()).isEqualTo(false);
+		assertThat(federer.getSets().size()).isEqualTo(3);
+		assertThat(federer.isWinner()).isEqualTo(false);
+		assertThat(rollandGarrosFinal.isFinished()).isEqualTo(true);
+		assertThat(scoreService.getScoreFor(rollandGarrosFinal))
+				.isEqualTo("Rafael Nadal/Roger Federer : 0-0 | 6/0 | 0/6 | 6/0 - Winner is : Rafael Nadal");
+	}
+
+	@Test
+	public void should_get_score_for_finished__party_when_player_win_6_4_and_5_7_and_6_1_games() throws ScoreException {
+		repeat(24, () -> {
+			try {
+				scoreService.score(rollandGarrosFinal, nadal);
+			} catch (ScoreException e) {
+				fail();
+			}
+		});
+		repeat(24, () -> {
+			try {
+				scoreService.score(rollandGarrosFinal, federer);
+			} catch (ScoreException e) {
+				fail();
+			}
+		});
+
+		repeat(20, () -> {
+			try {
+				scoreService.score(rollandGarrosFinal, nadal);
+			} catch (ScoreException e) {
+				fail();
+			}
+		});
+		repeat(4, () -> {
+			try {
+				scoreService.score(rollandGarrosFinal, nadal);
+			} catch (ScoreException e) {
+				fail();
+			}
+		});
+
+		assertThat(nadal.getGame().getScore()).isEqualTo(0);
+		assertThat(nadal.getSets().get(0).getScore()).isEqualTo(6);
+		assertThat(nadal.getSets().get(1).getScore()).isEqualTo(0);
+		assertThat(nadal.getSets().get(2).getScore()).isEqualTo(6);
+		assertThat(nadal.getSets().get(0).isCurrent()).isEqualTo(false);
+		assertThat(nadal.getSets().get(1).isCurrent()).isEqualTo(false);
+		assertThat(nadal.getSets().get(2).isCurrent()).isEqualTo(false);
+		assertThat(nadal.getSets().size()).isEqualTo(3);
+		assertThat(nadal.isWinner()).isEqualTo(true);
+		assertThat(federer.getGame().getScore()).isEqualTo(0);
+		assertThat(federer.getSets().get(0).getScore()).isEqualTo(0);
+		assertThat(federer.getSets().get(1).getScore()).isEqualTo(6);
+		assertThat(federer.getSets().get(2).getScore()).isEqualTo(0);
+		assertThat(federer.getSets().get(0).isCurrent()).isEqualTo(false);
+		assertThat(federer.getSets().get(1).isCurrent()).isEqualTo(false);
+		assertThat(federer.getSets().get(2).isCurrent()).isEqualTo(false);
+		assertThat(federer.getSets().size()).isEqualTo(3);
+		assertThat(federer.isWinner()).isEqualTo(false);
+		assertThat(rollandGarrosFinal.isFinished()).isEqualTo(true);
+		assertThat(scoreService.getScoreFor(rollandGarrosFinal))
+				.isEqualTo("Rafael Nadal/Roger Federer : 0-0 | 6/0 | 0/6 | 6/0 - Winner is : Rafael Nadal");
+	}
+
+	@Test
+	public void should_raise_exception_when_printing_score_for_null_party() {
+		assertThatThrownBy(() -> scoreService.getScoreFor(null)).isInstanceOf(ScoreException.class);
+	}
+
 	private void repeat(int times, Runnable runnable) {
 		range(0, times).forEach(i -> runnable.run());
 	}
